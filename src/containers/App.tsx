631bec4ab1e27b214ee66d69c9cbe30d020e99bc
James Kozianski <james@helixta.com.au>
2017-10-07 08:49:37 +1100

    Add margin-top to App.

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
 
+import { StyleSheet, css } from "aphrodite";
+
 import NewUserButton from "./NewUserButton";
 import UserList from "./UserList";
 
+const styles = StyleSheet.create({
+  container: {
+    marginTop: "2em",
+  },
+});
+
 export default (props: {}) => (
-  <div>
+  <div className={css(styles.container)}>
     <UserList />
     <NewUserButton />
   </div>
