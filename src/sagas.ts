fd4daadd900d41e4a68b40a11fc1c86b3bcae751
James Kozianski <james@helixta.com.au>
2017-10-15 10:18:31 +1100

    Use saga helper rather than bind().

--- src/sagas.ts
+++ src/sagas.ts
@@ -33,7 +33,7 @@ export function* watchInitialLoad(api: ApiServiceApi) {
 }
 
 function* handleInitialLoad(api: ApiServiceApi) {
-  const data: ApiFetchAllResponse = yield call(api.fetchAll.bind(api));
+  const data: ApiFetchAllResponse = yield call([api, api.fetchAll]);
   // We do a risky cast here because the types just "happen" to line up, except that the client's
   // fields aren't nullable. We don't expect the server to ever send null fields though.
   // TODO(james): Check for nullables.
