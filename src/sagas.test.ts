7daae63baece7bc807469476c19c497fc4ddb168
James Kozianski <james@helixta.com.au>
2017-10-15 12:06:13 +1100

    Add more generic async promise testing stuff.

--- src/sagas.test.ts
+++ src/sagas.test.ts
@@ -1,5 +1,5 @@
 import { Action, applyMiddleware, compose, createStore } from "redux";
-import { call, put, takeEvery } from "redux-saga/effects";
+import { call, put, select, takeEvery } from "redux-saga/effects";
 
 import createSagaMiddleware from "redux-saga";
 
@@ -8,6 +8,26 @@ export type Stublet<T> = {
   resolve(value?: any): void;
   reject(value?: any): void;
 };
+
+class SyncPromise {
+  private thens: Function[] = [];
+  private resolved = false;
+
+  resolve(value: any): void {
+    this.thens.forEach(f => f(value));
+    this.resolved = true;
+  }
+
+  then(f: Function): SyncPromise {
+    this.thens.push(f);
+    return this;
+  }
+
+  catch(f: Function): SyncPromise {
+    // TODO(james): Implement.
+    return this;
+  }
+}
 
 export type Stub<T> = T & { [P in keyof T]: Stublet<T[P]> };
 
@@ -98,6 +118,9 @@ describe("test sagas", () => {
 
     function* callServer(api: Api) {
       const result = yield call([api, api.doSomething]);
+      // Do an arbitrary select to ensure that it doesn't introduce async
+      // delays.
+      yield select(state => state);
       if (result) {
         yield put({ type: "TRUE" });
       } else {
@@ -113,8 +136,110 @@ describe("test sagas", () => {
     store.dispatch({ type: "START" });
     expect(store.getState()).toBe("START");
     mockApi.doSomething.resolve(true);
+    // The standard implementation of promises are inherently async, so need to
+    // wait for an event loop before sync actions chained after are done.
     await delay();
     expect(store.getState()).toBe("TRUE");
+  });
+});
+
+describe("promise tests", () => {
+  it("async chains in promises", async () => {
+    let res;
+    const promise = new Promise(resolve => {
+      res = resolve;
+    });
+
+    let p1 = false;
+    let p2 = false;
+    let p3 = false;
+    promise
+      .then(() => {
+        p1 = true;
+        return 5;
+      })
+      .then(() => {
+        p2 = true;
+        return new Promise(res => res());
+      })
+      .then(() => {
+        p3 = true;
+        return true;
+      });
+
+    expect(p1).toBe(false);
+    expect(res).not.toBeNull();
+    if (!res) {
+      throw "error";
+    }
+    res();
+    // Promises are inherently async, so need to wait before their .then()s run.
+    expect(p1).toBe(false);
+    await delay();
+    // But resolved promises and values don't cause any extra async ticks.
+    expect(p1).toBe(true);
+    expect(p2).toBe(true);
+    expect(p3).toBe(true);
+  });
+
+  it("resolved promises", async () => {
+    let res;
+    const resolvedAfter = new Promise(resolve => {
+      res = resolve;
+    });
+    if (!res) {
+      throw "error";
+    }
+    res();
+
+    // Even if the .then() is attached after the promise is resolved, we still
+    // need to wait.
+    let resolvedAfterDone = false;
+    resolvedAfter.then(() => (resolvedAfterDone = true));
+    expect(resolvedAfterDone).toBe(false);
+    await delay();
+    expect(resolvedAfterDone).toBe(true);
+
+    // Even if the promise is resolved in its own constructor, it still waits
+    // for a tick.
+    const resolvedInConstructor = new Promise(resolve => resolve());
+    let resolvedInConstructorDone = false;
+    resolvedInConstructor.then(() => {
+      resolvedInConstructorDone = true;
+    });
+    expect(resolvedInConstructorDone).toBe(false);
+    await delay();
+    expect(resolvedInConstructorDone).toBe(true);
+  });
+
+  it("async functions", async () => {
+    async function foo() {
+      return 5;
+    }
+
+    let done = false;
+    foo().then(() => {
+      done = true;
+    });
+    // async functions work the same way as new Promise().
+    expect(done).toBe(false);
+    await delay();
+    expect(done).toBe(true);
+  });
+
+  it("custom sync promises", async () => {
+    const promise = new SyncPromise();
+
+    let done = false;
+    promise.then(() => {
+      done = true;
+    });
+    // Obviously, if we write our own sync implementation of promises, then we
+    // can make it behave however we want. It may be worth implementing promises
+    // in this way so we don't have to remember to delay everywhere after we
+    // resolve a mocked promise.
+    promise.resolve(true);
+    expect(done).toBe(true);
   });
 });
 
