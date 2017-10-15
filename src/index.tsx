265d623a1a2bb56dac73529a5c91217c7364c1cb
James Kozianski <james@helixta.com.au>
2017-10-15 17:45:07 +1100

    Move App2 to App.

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import {
 } from "./sagas";
 
 import { ApiServiceApi } from "./backend/api";
-import App2 from "./containers/App2";
+import App from "./containers/App";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 import { initialLoadStart } from "./actions";
@@ -46,7 +46,7 @@ store.dispatch(initialLoadStart());
 
 ReactDOM.render(
   <Provider store={store}>
-    <App2 />
+    <App />
   </Provider>,
   document.getElementById("root") as HTMLElement
 );
